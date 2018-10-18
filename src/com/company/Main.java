7def3e1886e429e127658468be7e3e34732b712f
tkocher621 <42099068+tkocher621@users.noreply.github.com>
2018-10-17 21:02:38 -0400

    Update Main.java

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -1,8 +1,28 @@
 package com.company;
+import java.util.*;
 
 public class Main {
 
     public static void main(String[] args) {
-	// write your code here
+
+        Scanner sc = new Scanner (System.in);
+        String inp, sp[];
+
+        while (true)
+        {
+            System.out.println("Type in a sentence and press ENTER.");
+            inp = sc.nextLine();
+            inp = inp + "X";
+            inp = inp.toUpperCase();
+
+            sp = inp.split("S\\s*A");
+            if (inp.equals("EXITX"))
+                break;
+            System.out.println("There are " + (sp.length-1) + " occurrences.");
+            System.out.println();
+        }
+
+        System.out.println("Finished");
+
     }
 }
